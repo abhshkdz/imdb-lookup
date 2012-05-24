900123592bfe91f9b5aaa5fa6ae09c2c050dcdc8
Abhishek Das <das.abhshk@gmail.com>
2012-05-25 04:27:29 +0530

    improved styling

--- assets/js/search.js
+++ assets/js/search.js
@@ -18,7 +18,6 @@ $(document).ready(function() {
 
 	searchButton.on('click', function() {
 		console.log (searchText.val());
-		//progressBar.show('slow');
 		movie.addClass('hidden');
 		progressBar.removeClass('hidden');
 		search();
@@ -29,6 +28,7 @@ $(document).ready(function() {
 		var query = searchText.val();
 		if (query.length<3 && query.length>0) {
 			searchText.val('').attr("placeholder", "please enter longer query");
+			progressBar.addClass('hidden');
 			return;
 		}
 
@@ -37,14 +37,18 @@ $(document).ready(function() {
 		//$.getJSON ("http://www.imdbapi.com/?t="+query, function(data) {
 		$.getJSON ("./test/search.php?search="+query, function(data) {	
 			console.log (data);
-			//if (data.Response=="False") searchText.val('').attr("placeholder", "no results found");
-			//else {
-				//movie.show('slow');
-				movie.removeClass('hidden');
+			if (data.Response=="False") {
+				searchText.val('').attr("placeholder", "no results found");
 				progressBar.addClass('hidden');
+			}
+			else {
+				//movie.show('slow');
+				//movie.removeClass('hidden');
+				
+				//movie.css('visibility', 'hidden');
 				//progressBar.hide('slow');
 				movieTitle.html(data.Title);
-				moviePoster.attr('src', data.Poster);
+				moviePoster.attr('src', data.Poster)
 				plot.html(data.Plot);
 				genre.html(data.Genre);
 				director.html(data.Director);
@@ -52,7 +56,17 @@ $(document).ready(function() {
 				runtime.html(data.Runtime);
 				releaseDate.html(data.Released);
 				rating.html(data.Rated);
-			//}
+				moviePoster.on('load', function() {
+					movie.removeClass('hidden').css('opacity','0');
+					progressBar.addClass('hidden');
+
+					movie.animate({
+						'opacity': '1'
+					}, 'slow');
+					//movie.show('slow');
+					//progressBar.hide('slow');
+				});
+			}
 		});
 	}
 });	
